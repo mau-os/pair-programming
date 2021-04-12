912f2f3dcd277da64e55ce597ba48b8088ca97ca
Mauricio Oliveira <mau-os@hotmail.com>
2021-04-12 17:46:52 -0300

    Primeiro pair programming

--- lib/word_hunter.rb
+++ lib/word_hunter.rb
@@ -1,2 +1,16 @@
 class WordHunter
+  def self.crosswords(matrix, words)
+    splited_words = words.split
+    captured_words = []
+    result = []
+    complete_matrix = matrix + matrix.transpose
+    complete_matrix.each do |array|
+      captured_words << array.join
+      captured_words << array.join.reverse
+    end
+
+    result = captured_words & splited_words
+    
+    result.sort.join(' ')
+  end
 end
--- spec/word_hunter_spec.rb
+++ spec/word_hunter_spec.rb
@@ -6,5 +6,16 @@ describe 'WordHunter' do
               ['J', 'U', 'S'],
               ['N', 'G', 'S']]
     words = 'BUG'
+
+    expect(WordHunter.crosswords(matrix, words)).to eq 'BUG'
+  end
+
+  it 'Espera que retorne em ordem alfabética' do
+    matrix = [['J', 'U', 'S'],
+              ['B', 'U', 'G'],
+              ['N', 'G', 'S']]
+    words = 'SUJ BUG'
+
+    expect(WordHunter.crosswords(matrix, words)).to eq 'BUG SUJ'
   end
 end
